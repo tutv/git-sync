7d8f17508eae358c452b1e5e67469d76425a51cf
Tu TV <tutv95@gmail.com>
2015-11-20 00:00:01 +0700

    Test git sync 1 222

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 542_545454</div>
+                <div class="title">Laravel 542_5fsd45454</div>
             </div>
         </div>
     </body>
